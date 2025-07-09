8ede76c1e64899e26ca394daef9ce945a654436f
hakoom911 <114762298+hakoom911@users.noreply.github.com>
2025-07-09 22:46:07 +0300

    feat: adding error-middleware file to error-handler package and complete configuring auth-service app with some updates on tsconfig.app.json file of it.

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from "cors";
+import { errorMiddleware } from '../../../packages/error-handler/error-middleware';
+import cookieParser from 'cookie-parser';
 
 
 const app = express();
@@ -9,10 +11,14 @@ app.use(cors({
     allowedHeaders: ['Authorization', 'Content-Type'],
     credentials: true
 }))
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API' });
 });
+
+app.use(errorMiddleware)
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 6001;
@@ -20,6 +26,6 @@ const server = app.listen(port, host, () => {
     console.log(`Auth service is running at http://${host}:${port}/api`)
 })
 
-server.on("error",(e) => {
+server.on("error", (e) => {
     console.log("Server Error:", e)
 })
---
+++ packages/error-handler/error-middleware.ts
@@ -0,0 +1,16 @@
+import { Response, Request } from "express";
+import { AppError } from ".";
+
+export const errorMiddleware = (err: Error, req: Request, res: Response) => {
+    if (err instanceof AppError) {
+        console.log(`Error ${req.method} ${req.url} - ${err.message}`);
+        return res.status(err.statusCode).json({
+            status: "error",
+            message: err.message,
+            result: !err.details ? {} : { ...err.details }
+        },)
+    }
+
+    console.log("Unhandled error : ", err);
+    return res.status(500).json({ error: "Something went wrong, please try again!" })
+}
