b236ff2110ac1ad7fe895f41ad01dd1253adaaf8
hakoom911 <114762298+hakoom911@users.noreply.github.com>
2025-07-09 21:57:07 +0300

    feat: add first setup for the auth-service app.

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,25 @@
 import express from 'express';
+import cors from "cors";
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
 
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true
+}))
+
 app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
+    res.send({ 'message': 'Hello API' });
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
-});
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+const server = app.listen(port, host, () => {
+    console.log(`Auth service is running at http://${host}:${port}/api`)
+})
+
+server.on("error",(e) => {
+    console.log("Server Error:", e)
+})
