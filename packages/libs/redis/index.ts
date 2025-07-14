e19f1104fb9f3effd4a83d2a843afb807b7b753b
hakoom911 <114762298+hakoom911@users.noreply.github.com>
2025-07-14 21:00:36 +0300

    refactor: update the env var name of redis database connection key.

--- packages/libs/redis/index.ts
+++ packages/libs/redis/index.ts
@@ -1,10 +1,5 @@
 import Redis from "ioredis";
 
-// const redis = new Redis({
-//     host: process.env.REDIS_HOST || "127.0.0.1",
-//     port: Number(process.env.REDIS_PORT) || 6379,
-//     password: process.env.REDIS_PASSWORD
-// }); 
-const redis = new Redis(process.env.REDIS_TCP_URL || "");
+const redis = new Redis(process.env.REDIS_DATABASE_URL || "");
 
 export default redis;
