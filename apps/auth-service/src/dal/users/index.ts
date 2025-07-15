306c87d73cdc3418df421e528199c9373c286ebc
hakoom911 <114762298+hakoom911@users.noreply.github.com>
2025-07-15 23:02:22 +0300

    refactor: update some codes.

--- apps/auth-service/src/dal/users/index.ts
+++ apps/auth-service/src/dal/users/index.ts
@@ -14,6 +14,15 @@ export async function createUser(data: Prisma.usersCreateInput) {
     try {
         return await prisma.users.create({ data })
     } catch (err) {
-        throw new Error("Error while create user")
+        throw new Error(`Error while creating user, ${(err as Error).message}`)
+    }
+}
+
+export async function updateUser(field: "id" | "email", value: string, data: Prisma.usersUpdateInput) {
+    try {
+        const f = field === "email" ? { email: value } : { id: value };
+        return await prisma.users.update({ where: { ...f }, data })
+    } catch (err) {
+        throw new Error(`Error while updating user, ${(err as Error).message}`)
     }
 }
--- apps/auth-service/src/libs/bcrypt/index.ts
+++ apps/auth-service/src/libs/bcrypt/index.ts
@@ -6,6 +6,6 @@ export async function hashPassword(password: string) {
     return await bcrypt.hash(password, PASSWORD_SALT)
 }
 
-export async function verifyPassword(password: string, hashedPassword: string) {
+export async function comparePassword(password: string, hashedPassword: string) {
     return await bcrypt.compare(password, hashedPassword);
 }
