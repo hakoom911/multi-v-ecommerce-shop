a40d7363ea5a9dd5fb205cd9baecba4c21fe6866
hakoom911 <114762298+hakoom911@users.noreply.github.com>
2025-07-15 20:59:25 +0300

    refactor: remove un need data from user login return response payload.

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -82,7 +82,7 @@ export async function loginUser(req: Request, res: Response, next: NextFunction)
         res.status(200).json({
             message: "User login successfully!",
             result: {
-                user: { id: user.id, name: user.name, email: user.email , accessToken, refreshToken }
+                user: { id: user.id, name: user.name, email: user.email }
             }
         })
 
